1b3ed68d24b48503fe26616d7cfce22c1f6dbef7
adarsh <adarshkumar1892005@gmail.com>
2026-01-21 22:04:26 +0530

    added questions on arrays

--- c++ with dsa/array/reverse_array.cpp
+++ c++ with dsa/array/reverse_array.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 void reversearray(int arr[], int n)
@@ -26,8 +25,7 @@ int main()
 {
     int arr[6] = {1, 2, 3, 4, 5, 6};
     printarray(arr, 6);
-    // reversearray(arr, 6);
-    std::reverse(arr, arr + 6);
+    reversearray(arr, 6);
     printarray(arr, 6);
     return 0;
 }
