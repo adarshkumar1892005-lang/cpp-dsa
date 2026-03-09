54d1f206ba980fbe8e9c9401fea6f1bffc61c0a6
adarsh <adarshkumar1892005@gmail.com>
2026-03-09 20:08:13 +0530

    added circular queue and modified queue using array and linked list

--- data structures/unit 1/10_circular_queue.cpp
+++ data structures/unit 1/10_circular_queue.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 #define N 5
 int queue[N];
-int front = -1;
 int rear = -1;
+int front = -1;
 
 void enqueue(int x)
 {
-    if (rear == N - 1)
+    if (rear == -1 && front == -1)
+    {
+        rear = 0;
+        front = 0;
+        queue[rear] = x;
+    }
+    else if ((rear + 1) % N == front)
     {
         cout << "overflow condition\n";
     }
-    else if (front == -1 && rear == -1)
-    {
-        rear++;
-        front++;
-        queue[rear] = x;
-    }
     else
     {
-        rear++;
+        rear = (rear + 1) % N;
         queue[rear] = x;
     }
 }
@@ -38,8 +38,7 @@ void dequeue()
     }
     else
     {
-        cout << "deleted element is " << queue[front] << endl;
-        front++;
+        front = (front + 1) % N;
     }
 }
 
@@ -49,9 +48,10 @@ void peek()
     {
         cout << "underflow condition\n";
     }
+
     else
     {
-        cout << "element at front is " << queue[front] << endl;
+        cout << "the element at rear is "<< " " << queue[rear] << endl;
     }
 }
 
@@ -63,25 +63,27 @@ void display()
     }
     else
     {
-        for (int i = front; i <= rear; i++)
+        cout << "the queue is :- \n";
+        for (int i = front; i != rear; i = (i + 1) % N)
         {
             cout << queue[i] << " ";
         }
+        cout << queue[rear];
         cout << endl;
     }
 }
 
-int main()
-{
-    dequeue();
-    enqueue(3);
-    enqueue(2);
-    enqueue(1);
-    display();
-    dequeue();
-    dequeue();
-    dequeue();
-    peek();
-
-    return 0;
+int main() {
+    enqueue(5);
+    enqueue(6);
+    dequeue();          // remove 5
+    enqueue(7);
+    enqueue(8);
+    dequeue();          // remove 6
+    display();          // check order of remaining elements
 }
+
+
+
+
+
--- data structures/unit 2/queue_using_ll.cpp
+++ data structures/unit 2/queue_using_ll.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 using namespace std;
 
+// implementation of queue using linked list //
 struct node
 {
     int data;
     node *next;
 };
-node *front = 0;
-node *rear = 0;
+node *front = 0; // front as head
+node *rear = 0; // rear as tail
 
-void enqueue(int x)
+void enqueue(int x) // for adding element in queue
 {
-    node *newnode = new node;
+    node *newnode = new node; // initialising newnode
     newnode->data = x;
     newnode->next = 0;
-    if (front == 0 && rear == 0)
+    if (front == 0 && rear == 0)  // for empty queue 
     {
         front = newnode;
         rear = newnode;
     }
-    else
+    else //if queue is not empty 
     {
         rear->next = newnode;
         rear = newnode;
     }
 }
 
-void dequeue()
+void dequeue()  // for deleting element at front
 {
-    if (front == 0 && rear == 0)
+    if (front == 0 && rear == 0) // for underflow condition
     {
         cout << "underflow condition\n";
     }
@@ -37,18 +38,18 @@ void dequeue()
         node *temp = front;
         cout << "deleted element is " << front->data << endl;
         front = front->next;
-        if (front == 0)
+        if (front == 0) // for setting rear to 0 if if queue is empty
         {
             rear =0;
         }
         
-        delete temp;
+        delete temp; // for deleting element at front
     }
 }
 
-void seek()
+void seek()  // to display the rear element
 {
-    if (front == 0 && rear == 0)
+    if (front == 0 && rear == 0)  // for checking underflow condition
     {
         cout << "underflow condition\n";
     }
@@ -58,13 +59,13 @@ void seek()
     }
 }
 
-void display()
-{   if (front ==0 && rear == 0)
+void display() //for displaying queue
+{   if (front ==0 && rear == 0)  // checking underflow condition
     {
         cout << "underflow condition\n";
     }
     else
-    {node* temp = front;
+    {
     cout << "the queue is :- \n";
     for (node *temp = front; temp != 0; temp = temp -> next)
     {
@@ -77,13 +78,11 @@ int main()
 {
     enqueue(3);
     enqueue(2);
-    enqueue(6);
-    display();
-    dequeue();
     seek();
     dequeue();
     dequeue();
     display();
+    seek();
 
 
     return 0;
